405019394e8b35fc739f717e63cbf714919662dd
Samuel Shirley <37374497+samuelshirley@users.noreply.github.com>
2020-06-25 12:57:28 -0600

    Delete a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -225,4 +225,41 @@ router.post(
   }
 );
 
+//@route          DELTE /api/posts/comment/:id/:commentId
+//@description    Remove a commnet from a post
+//@access         Private
+router.delete('/comment/:id/:comment_Id', auth, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_Id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User unauthorized' });
+    }
+
+    const removeIndex = post.comments.map((comment) =>
+      comment.user.toString().indexOf(req.user.id)
+    );
+
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
